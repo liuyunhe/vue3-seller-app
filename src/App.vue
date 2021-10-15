f5a142b4e0963fee9a355aa03e86910790ed1d09
liuyunhe <liuyunhe666@aliyun.com>
2021-10-15 11:09:09 +0800

    分享fix

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,9 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent, onMounted } from 'vue'
 import { useStore } from 'vuex'
+import { initWxOnReady, wxShare } from './plugins/Wx'
 import { GlobalDataProps } from './store'
 
 export default defineComponent({
@@ -16,6 +17,19 @@ export default defineComponent({
     const isLoading = computed(() => store.state.loading)
     const token = computed(() => store.state.token)
     sessionStorage.setItem('token', token.value)
+    onMounted(() => {
+      if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+        initWxOnReady(() => {
+          wxShare({
+            shareUrl: '/orgmenu/auth?menuCode=sellerFansBind',
+            shareTitle: '分享好友绑定粉丝',
+            shareDesc: '分享好友绑定粉丝',
+            shareImg:
+              'https://qrmkt.oss-cn-beijing.aliyuncs.com/hbseller_client/building-icon.png'
+          })
+        })
+      }
+    })
     return {
       isLoading
     }
