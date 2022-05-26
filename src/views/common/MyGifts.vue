425639c5eb05420c9c451b1f400e255501fd4547
liuyunhe <liuyunhe666@aliyun.com>
2022-05-26 17:23:52 +0800

    奖品列表

--- src/views/common/MyGifts.vue
+++ src/views/common/MyGifts.vue
@@ -1,76 +1,127 @@
 <template>
   <div class="myGifts-container">
-    <template v-if="giftsList !== null && giftsList.length === 0">
+    <van-list
+      v-model:loading="loading"
+      :finished="finished"
+      :finished-text="finishedText"
+      @load="onListLoad"
+      :offset="10"
+    >
+      <template v-for="item in giftsList" :key="item.id">
+        <div class="lp-item" @click="getReceive(item)">
+          <div class="lp-item-borad">
+            <div
+              class="img-zone"
+              :style="{ backgroundImage: `url(${item.awdPic})` }"
+            ></div>
+            <div class="content-zone">
+              <div class="name">{{ item.awdName }}</div>
+              <div class="name" style="font-size: 0.32rem">
+                {{ item.actName }}
+              </div>
+              <div class="tips" v-if="item.awdType != 3 && item.awdStatus == 0">
+                请在7天内领取
+              </div>
+              <div class="tips" v-if="item.awdType == 3 && item.awdStatus == 0">
+                请在24小时内领取
+              </div>
+            </div>
+            <div class="receive-btn" v-if="item.awdStatus == 0">待领取</div>
+            <div class="receive-btn used" v-if="item.awdStatus == 1">
+              已领取
+            </div>
+            <div class="receive-btn used" v-if="item.awdStatus == 2">
+              已过期
+            </div>
+          </div>
+          <div class="lp-item-func">
+            <div class="time">中奖时间：{{ item.drawTime }}</div>
+            <div
+              v-if="item.awdType == 1 && item.awdStatus != 2"
+              class="btn"
+              @click="jumpToOrder(item.uaId)"
+            >
+              订单详情
+            </div>
+          </div>
+          <div class="disabled" v-if="item.awdStatus == 2"></div>
+        </div>
+      </template>
+    </van-list>
+    <div class="h30"></div>
+    <template v-if="showNoGift">
       <div class="page-no-gift"></div>
       <div class="no-gift-tips">暂无礼品</div>
     </template>
+    <template v-else></template>
   </div>
 </template>
 
 <script lang="ts">
 import { http } from '@/http'
+import { DrawData, handleReceive } from '@/plugins/hbsDraw'
 import { Toast } from 'vant'
-import { defineComponent, onMounted, ref } from 'vue'
-interface Gift {
-  actCode: string
-  actName: string
-  awardCode: string
-  awardId: number
-  awdArea: string
-  awdCity: string
-  awdDetailType: number
-  awdDistrict: string
-  awdJumpurl: string | null
-  awdName: string
-  awdPic: string
-  awdPrice: string
-  awdProvince: string
-  awdStatus: number
-  awdType: number
-  cardValue: number
-  code: string
-  ctime: string
-  drawTime: string
-  expireTime: string
-  giveIntegral: number
-  id: number
-  integral: number
-  orgId: string
-  prizeName: string
-  redMoney: number
-  sn: string
-  uaId: string
-  userId: number
-}
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'MyGifts',
   setup() {
-    const giftsList = ref<Array<Gift> | null>(null)
-    const getMyGiftsDetail = () => {
-      const params = {
-        recordId: 0,
-        size: 10,
-        actTypeMain: 2
-      }
-      http
-        .post('/syx/awd/list/part', params, false)
-        .then((res) => {
-          if (res.code === '200') {
-            giftsList.value = res.data
-          } else {
-            Toast.fail(res.msg)
-          }
-        })
-        .then((err) => {
-          console.log(err)
-        })
+    const loading = ref(false)
+    const finished = ref(false)
+    const showNoGift = ref(false)
+    const finishedText = ref('没有更多了')
+    const giftsList = ref<Array<DrawData>>([])
+    const params = {
+      recordId: 0,
+      size: 10
     }
-    onMounted(() => {
+    const getMyGiftsDetail = () => {
+      http.get('/syx/awd/list', params).then((res) => {
+        if (res.code === '200') {
+          if (res.data.length < params.size) {
+            if (params.recordId == 1 && res.data.length == 0) {
+              finishedText.value = ''
+              showNoGift.value = true
+            }
+            // 数据全部加载完成
+            finished.value = true
+            giftsList.value.push(...res.data)
+          } else {
+            // 加载状态结束
+            loading.value = false
+            giftsList.value.push(...res.data)
+          }
+          giftsList.value.push(...res.data)
+        } else {
+          Toast.fail(res.msg)
+        }
+      })
+    }
+    const onListLoad = () => {
+      if (giftsList.value.length - 1 > 0) {
+        params.recordId = giftsList.value[giftsList.value.length - 1].id
+      }
       getMyGiftsDetail()
-    })
+    }
+    const getReceive = (item: DrawData) => {
+      if (item.awdStatus == 0) {
+        handleReceive(item, () => {
+          console.log('received')
+        })
+      }
+    }
+    const jumpToOrder = (id: string) => {
+      window.location.href = `/yx/views/general/order-detail.html?uaId=${id}&&hbSeller=1`
+    }
     return {
-      giftsList
+      loading,
+      finished,
+      showNoGift,
+      finishedText,
+      giftsList,
+      onListLoad,
+      jumpToOrder,
+      getReceive
     }
   }
 })
@@ -80,6 +131,8 @@ export default defineComponent({
 .myGifts-container {
   height: 100vh;
   background-color: #f4f4f4;
+  overflow: scroll;
+  padding-top: 16px;
   &:before {
     content: '.';
     display: block;
@@ -101,6 +154,121 @@ export default defineComponent({
     line-height: 14px;
     text-align: center;
     color: #b1b1b1;
+  }
+  .lp-item {
+    position: relative;
+    margin: 16px auto;
+    width: 343px;
+    height: 141px;
+    padding: 0 10px;
+    background-image: url(https://qrmkt.oss-cn-beijing.aliyuncs.com/common/memberDay/gift_bg_normal.png);
+    background-repeat: no-repeat;
+    background-size: 100%;
+    box-sizing: border-box;
+    &:first-child {
+      margin-top: 0;
+    }
+    .disabled {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      background-image: url(https://qrmkt.oss-cn-beijing.aliyuncs.com/common/memberDay/gift_bg_disabled.png);
+      background-repeat: no-repeat;
+      background-size: 100%;
+    }
+    .lp-item-borad {
+      width: 100%;
+      height: 100px;
+      padding-top: 20px;
+      position: relative;
+      box-sizing: border-box;
+      .img-zone {
+        width: 51px;
+        height: 61px;
+        margin-left: 17px;
+        float: left;
+        background-image: url(https://qrmkt.oss-cn-beijing.aliyuncs.com/common/memberDay/hb.png);
+        background-repeat: no-repeat;
+        background-size: 100%;
+        background-position: center;
+        background-size: contain;
+      }
+      .content-zone {
+        margin-left: 80px;
+        .name {
+          width: 4.1rem;
+          line-height: 20px;
+          font-size: 14px;
+          color: #444;
+          margin-bottom: 7px;
+          overflow: hidden;
+          max-height: 40px;
+        }
+        .tips {
+          height: 20px;
+          line-height: 20px;
+          font-size: 12px;
+          color: #888888;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+      }
+      .receive-btn {
+        position: absolute;
+        width: 60px;
+        height: 24px;
+        right: 0;
+        top: 20px;
+        box-sizing: border-box;
+        border: 1px solid #ff7400;
+        border-radius: 2px;
+        text-align: center;
+        line-height: 24px;
+        font-size: 12px;
+        color: #ff7400;
+        &.used {
+          color: #7f7f7f;
+          border-color: #7f7f7f;
+        }
+      }
+    }
+    .lp-item-func {
+      height: 40px;
+      position: relative;
+      .time {
+        height: 40px;
+        line-height: 40px;
+        color: #888888;
+        font-size: 12px;
+        width: 250px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+      .btn {
+        position: absolute;
+        top: 8px;
+        right: 0;
+        width: 60px;
+        height: 24px;
+        background: linear-gradient(
+          0deg,
+          rgba(255, 150, 0, 1) 0%,
+          rgba(255, 114, 0, 1) 100%
+        );
+        border-radius: 2px;
+        text-align: center;
+        line-height: 24px;
+        font-size: 12px;
+        color: #fff;
+      }
+    }
+  }
+  .h30 {
+    height: 30px;
   }
 }
 </style>
